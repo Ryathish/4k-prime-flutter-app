c52cc7ca004880efc1b745ef0b0986c8a18c7259
Ryathish <34179132+Ryathish@users.noreply.github.com>
2025-04-14 07:16:09 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,80 +1,18 @@
-// 4K Prime Flutter App (WebView + Google Sign-In + Google Play Billing)
-
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(PrimeApp());
+  runApp(MyApp());
 }
 
-class PrimeApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '4K Prime',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: SignInScreen(),
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class SignInScreen extends StatefulWidget {
-  @override
-  _SignInScreenState createState() => _SignInScreenState();
-}
-
-class _SignInScreenState extends State<SignInScreen> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  GoogleSignInAccount? _user;
-
-  void _login() async {
-    try {
-      final user = await _googleSignIn.signIn();
-      if (user != null) {
-        setState(() => _user = user);
-        // TODO: Check for Google Play Billing subscription here
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => WebViewScreen()),
-        );
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _login,
-          child: Text('Sign in with Google'),
-        ),
-      ),
-    );
-  }
-}
-
-class WebViewScreen extends StatelessWidget {
-  final String websiteUrl = 'https://sites.google.com/view/4kprime/home';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("4K Prime")),
-      body: WebView(
-        initialUrl: websiteUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
-    );
-  }
-}
-
-// TODO:
-// 1. Integrate Google Play Billing plugin for subscription check
-// 2. Block access to WebView unless subscription is active
-// 3. Setup Firebase project and add Google Services JSON
-// 4. Configure Play Console with ₹50/month auto-renew plan
